cd3caf424899de931ba68b5411b43a0f41373abe
Riccardo Grigoletto <rgrigoletto@IIT.LOCAL@iiticubsrv018.iit.local>
2020-08-28 12:46:12 +0000

    integrate icwt 2

---
+++ test_voc.sh
@@ -0,0 +1,7 @@
+FILES=output/res50/voc0712/MC/*.pth
+
+for f in $FILES
+do
+        CUDA_VISIBLE_DEVICES=3 time python3.6 ./tools/test_net.py --imdb voc_2007_test --model $f --cfg experiments/cfgs/res50.yml --net res50 --set ANCHOR_SCALES [8,16,32] ANCHOR_RATIOS [0.5,1,2]
+done 
+
